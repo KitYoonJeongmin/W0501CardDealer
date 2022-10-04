e0ea5a06e9155915fc18b1f44a2f96592a6a4fb9
KitYoonJeongmin <102850131+KitYoonJeongmin@users.noreply.github.com>
2022-10-04 15:48:16 +0900

    [Create] getCardName 함수 구현 : int를 입력하면 해당 카드의 이름을 가져옴

--- app/src/main/java/kr/ac/kumoh/s20200759/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20200759/w0501carddealer/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20200759.w0501carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20200759.w0501carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //setContentView(R.layout.activity_main)
+        getCardName(29)
+        binding.card1.setImageResource(R.drawable.c_10_of_spades)
+    }
+    private fun getCardName(c: Int): String{
+        Log.i("getCardName() : ", (c).toString())
+        val shape = when (c/13){
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+        Log.i("getCardName() : ", shape.toString())
+        val number = when (c%13){
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        Log.i("getCardName() : ", number.toString())
+        return "c_${number}_of_${shape}"
     }
 }
